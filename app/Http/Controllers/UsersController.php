4c03f9bad7a67913bd9ff031354a5081990a07f6
walkinnight <liluhua233@gmail.com>
2020-09-13 15:53:18 +0000

    渲染用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -13,7 +13,7 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth',[
-           'except'=>['show','create','store']
+           'except'=>['show','create','store','index']
         ]);
 
         $this->middleware("guest",[
@@ -79,6 +79,12 @@ class UsersController extends Controller
 
         session()->flash('success','个人资料修改成功！');
         return redirect()->route('users.show', $user->id);
+    }
+
+    //用户列表
+    public function index(){
+        $users = User::paginate(10);
+        return view('users.index',compact('users'));
     }
 
 
