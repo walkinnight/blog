a7afc7b5c00eed761bd04dd6d33e7f67fbeb4e3d
walkinnight <liluhua233@gmail.com>
2020-09-13 04:36:58 +0000

    添加csrf验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,4 +16,16 @@ class UsersController extends Controller
     public function show(User $user){
         return view('users.show',compact('user'));
     }
+
+    //保存用户信息
+    public function store(Request $request){
+
+        $this->validate($request,[
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+
+    }
 }
