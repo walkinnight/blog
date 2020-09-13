b878b46e1e6e46bed5493140d4ecbb14de06f2a7
walkinnight <liluhua233@gmail.com>
2020-09-13 04:11:52 +0000

    完成用户页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
     //注册页面
     public function create(){
         return view('users.signup');
+    }
+
+    //用戶個人信息頁面
+    public function show(User $user){
+        return view('users.show',compact('user'));
     }
 }
