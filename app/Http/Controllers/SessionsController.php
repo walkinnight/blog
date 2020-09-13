c482be292b19f752444b5bb9baf65b3478577578
walkinnight <liluhua233@gmail.com>
2020-09-13 11:08:18 +0000

    退出

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -30,4 +30,12 @@ class SessionsController extends Controller
         }
     }
 
+    //退出
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect('login');
+    }
+
 }
