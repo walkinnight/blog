5efa4d6201815db684bec79fb0ce3aae708eb190
walkinnight <liluhua233@gmail.com>
2020-09-13 04:23:29 +0000

    修改个人注册页面视图名称为create

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,7 +9,7 @@ class UsersController extends Controller
 {
     //注册页面
     public function create(){
-        return view('users.signup');
+        return view('users.create');
     }
 
     //用戶個人信息頁面
