96ebc344db646ca4c9060ee202d6f2dd1ff51b7e
walkinnight <liluhua233@gmail.com>
2020-09-14 14:18:26 +0000

    关注按钮

--- routes/web.php
+++ routes/web.php
@@ -36,5 +36,10 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 //微博
 
 Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+
+
+//关注
 Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
 Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
+Route::post('/users/followers/{user}', 'FollowersController@store')->name('followers.store');
+Route::delete('/users/followers/{user}', 'FollowersController@destroy')->name('followers.destroy');
